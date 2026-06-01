2f03578f9326b618d556e2fc0111bd7cc8ca00bf
Phayserlpepe <phayserloumar@gmail.com>
2026-06-01 19:54:15 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -14,3 +14,52 @@ function addTransaction(e) {
   }
   
   const transaction = {
+const transaction = {
+    id: Math.floor(Math.random() * 100000),
+    text: text.value,
+    amount: +amount.value
+  };
+  
+  transactions.push(transaction);
+  updateLocalStorage();
+  init();
+  text.value = '';
+  amount.value = '';
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateBalance();
+}
+
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}$${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+  list.appendChild(item);
+}
+
+function updateBalance() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => acc += item, 0).toFixed(2);
+  balance.innerText = `$${total}`;
+}
+
+form.addEventListener('submit', addTransaction);
+init();
